001fcc029b6ec8680d487ea2954665c0de862ae0
Long Tran <lngtrn94@gmail.com>
2018-01-09 21:30:55 +0100

    node.js helloworld

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get("/hello", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.listen(port);
+console.log("Listening on port ", port);
+
+require("cf-deployment-tracker-client").track();
