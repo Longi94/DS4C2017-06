0d7eb10c35a57347ac5240e0cfeea0a2402baecd
Long Tran <lngtrn94@gmail.com>
2018-01-21 19:56:06 +0100

    python script to process songs

---
+++ analyze_songs.py
@@ -0,0 +1,83 @@
+import requests
+
+API_KEY = "f727868c408e5996298e495da0b1f1f6"
+
+
+def get_lyrics(id):
+    response = requests.get("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + id + "&apikey=" + API_KEY)
+
+    return response.json()['message']['body']['lyrics']['lyrics_body']
+
+
+def analyze_tones(lyrics):
+    response = requests.post('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
+                             json={'text': lyrics}, auth=('6b9d8b2a-b0e0-4ea6-8545-94dbc29ceb1b', 'UpcRHdvKpfC4'))
+
+    sentences = filter(lambda x: len(x['tones']) > 0, response.json()['sentences_tone'])
+
+    tones = {}
+
+    for sentence in sentences:
+        for tone in sentence['tones']:
+            if not tone['tone_id'] in tones:
+                tones[tone['tone_id']] = {
+                    'avg': tone['score'],
+                    'count': 1
+                }
+            else:
+                tones[tone['tone_id']]['avg'] = (tones[tone['tone_id']]['avg'] * tones[tone['tone_id']]['count'] +
+                                                 tone['score']) / (tones[tone['tone_id']]['count'] + 1)
+                tones[tone['tone_id']]['count'] += 1
+
+    for key, value in tones.items():
+        tones[key] = value['avg']
+
+    return tones
+
+
+def analyze_personality(lyrics):
+    body = lyrics + lyrics + lyrics
+
+    response = requests.post('https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-09-21',
+                             data=body.encode('utf-8'), auth=('a305ea75-d537-4a7b-80ed-ec43966043f4', 'cnnQB2MSqQGe'),
+                             headers={'Content-Type': 'text/plain; charset=UTF-8'})
+
+
+    personalities = {}
+
+    for personality in response.json()['personality']:
+        personalities[personality['trait_id']] = personality['percentile']
+
+    return personalities
+
+
+def save_song(song):
+    requests.post('https://api.us.apiconnect.ibmcloud.com/tltranstudentvunl-dev/sb/api/Songs',
+                  headers={
+                      'accept': 'application/json',
+                      'content-type': 'application/json',
+                      'x-ibm-client-secret': 'rD5lP8gW5qL4jB7fK7aS7dF0rP6nK4xD5fI2sO6bB4jW8eJ1tH',
+                      'x-ibm-client-id': 'eb4ea9ba-bd16-4789-bfd5-56620abc3d43'
+                  }, json=song)
+
+
+with open("backend/songs.csv", "r", encoding='utf-8') as f:
+    for line in f:
+        print("Processing " + line)
+
+        row = line[:-1].split('|')
+
+        lyrics = get_lyrics(row[0])
+
+        tones = analyze_tones(lyrics)
+
+        personalities = analyze_personality(lyrics)
+
+        song = {
+            'title': row[1],
+            'artist': row[2],
+            'personalities': personalities,
+            'tones': tones
+        }
+
+        save_song(song)
