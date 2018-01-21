30ab2255713a7dad6fad92d3b6cd548d28b88f44
Long Tran <lngtrn94@gmail.com>
2018-01-21 20:51:15 +0100

    fixeroni

--- analyze_songs.py
+++ analyze_songs.py
@@ -4,22 +4,34 @@ API_KEY = "f727868c408e5996298e495da0b1f1f6"
 
 
 def get_lyrics(id):
-    response = requests.get("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + id + "&apikey=" + API_KEY)
+    response = requests.get(
+        "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=f727868c408e5996298e495da0b1f1f6&track_id=" + id)
 
-    return response.json()['message']['body']['lyrics']['lyrics_body']
+    lyrics = response.json()['message']['body']['lyrics']['lyrics_body']
+
+    lyrics = lyrics[:lyrics.find('\n...\n\n*******')]
+
+    return lyrics
 
 
 def analyze_tones(lyrics):
     response = requests.post('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
                              json={'text': lyrics}, auth=('6b9d8b2a-b0e0-4ea6-8545-94dbc29ceb1b', 'UpcRHdvKpfC4'))
 
-    sentences = filter(lambda x: len(x['tones']) > 0, response.json()['sentences_tone'])
+    result_json = response.json()
 
     tones = {}
 
+    if 'sentences_tone' not in result_json:
+        for tone in result_json['document_tone']['tones']:
+            tones[tone['tone_id']] = tone['score']
+        return tones
+
+    sentences = filter(lambda x: len(x['tones']) > 0, result_json['sentences_tone'])
+
     for sentence in sentences:
         for tone in sentence['tones']:
-            if not tone['tone_id'] in tones:
+            if tone['tone_id'] not in tones:
                 tones[tone['tone_id']] = {
                     'avg': tone['score'],
                     'count': 1
@@ -36,12 +48,15 @@ def analyze_tones(lyrics):
 
 
 def analyze_personality(lyrics):
-    body = lyrics + lyrics + lyrics
+    body = lyrics
 
-    response = requests.post('https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-09-21',
-                             data=body.encode('utf-8'), auth=('a305ea75-d537-4a7b-80ed-ec43966043f4', 'cnnQB2MSqQGe'),
-                             headers={'Content-Type': 'text/plain; charset=UTF-8'})
+    while len(body.split()) < 150:
+        body += lyrics
 
+    response = requests.post(
+        'https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-09-21',
+        data=body.encode('utf-8'), auth=('a305ea75-d537-4a7b-80ed-ec43966043f4', 'cnnQB2MSqQGe'),
+        headers={'Content-Type': 'text/plain; charset=UTF-8'})
 
     personalities = {}
 
