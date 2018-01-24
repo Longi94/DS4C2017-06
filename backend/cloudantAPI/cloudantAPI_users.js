205ccb171c2d7289870cc7ed9b257a077c0e13cc
Konrad Karaś <knrdkrs@gmail.com>
2018-01-24 17:51:08 +0100

    feed creation: code cleanup

--- backend/cloudantAPI/cloudantAPI_users.js
+++ backend/cloudantAPI/cloudantAPI_users.js
@@ -83,33 +83,6 @@ class CloudantAPI_Users {
 
         });
     }
-
-    createFeed(songId, accessToken, callback) {
-        var header =  { 
-            accept: 'application/json',
-            'content-type': 'application/json',
-            'x-ibm-client-secret': 'rD5lP8gW5qL4jB7fK7aS7dF0rP6nK4xD5fI2sO6bB4jW8eJ1tH',
-            'x-ibm-client-id': 'eb4ea9ba-bd16-4789-bfd5-56620abc3d43',
-            'Authorization': accessToken
-        };
-        var options = {
-            method: 'PUT',
-            url: 'https://api.us.apiconnect.ibmcloud.com/tltranstudentvunl-dev/sb/api/Clients/me/songs/rel/' + songId + '?access_token=$ACCESS_TOKEN',
-            headers: header,
-            json: true
-        };
-
-        console.log(options);
-
-        request(options, function (error, response, body) {
-          if (error) callback(null, error);
-
-          console.log('Success: ', body);
-
-          callback(null, body);
-
-        });
-    }
 }
 
 
